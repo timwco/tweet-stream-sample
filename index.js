7757ee6495a0c6d299e2db6fcdbceb5414f6b388
Tim Whitacre <tim@timw.co>
2016-06-17 10:08:09 -0400

    updated messages

--- index.js
+++ index.js
@@ -17,25 +17,28 @@ const allowCrossDomain = function(req, res, next) {
 };
 
 // Random Tweet Generator
-const names     = ['sam', 'greg', 'bill', 'stacy', 'juli', 'jef', 'gary', 'betty', 'betsy', 'blake'];
-const tags      = ['tech', 'ponce', 'pizza'];
+const names     = ['tech404', 'atl-tech', 'bill_jeffers', 'lovehotweather', 'atleats', 'southernbell123', 'loveblackcoffee', 'atl4life', 'gatech2015', 'southernborn555'];
+const tags      = ['tech', 'ponce', 'pizza', 'theironyard'];
 const genders   = ['men', 'women'];
+const messages  = ['Congrats to all the graduates!', 'Lots of awesome projects today @theironyard Atlanta demo day!', 'Good luck to all the iron yard demo day presentors', 'About to watch some awesome presentations tonight @theironyard Atlanta', 'Looking to hire some developers, come check out TIY Atlanta\'s demo day!'];
 
 function generateTweet () {
-  let rand1 = _.random(0, 12);
+  let rand1 = _.random(12);
   let rand2 = _.random(5, 50);
-  let rand3 = _.random(0, 1);
+  let rand3 = _.random(1);
+  let rand4 = _.random(4);
 
   let name = names[rand1];
   let tag  = tags[rand1];
   let gender = genders[rand3];
   let photoId = rand2;
+  let message = messages[rand4];
 
-  name = name ? name : 'sally';
+  name = name ? name : 'sallyTweets';
   tag  = tag ? '#' + tag : '';
 
   return {
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. De quibus cupio scire quid sentias. Negare non possum. ' + tag,
+    text: message + tag,
     user: {
       name: name,
       profile_image_url: 'https://randomuser.me/api/portraits/' + gender + '/' + photoId + '.jpg'
